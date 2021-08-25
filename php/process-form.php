0fba94242e3a9c7243b6353951c9aec85a6621cf
*esomagsorc894* <you@example.com>
2021-08-25 16:15:39 -0400

    contact form

--- php/process-form.php
+++ php/process-form.php
@@ -1,19 +1,20 @@
-<?php
-if (isset($_REQUEST['name'],$_REQUEST['email'])) {
+
+
+<!-- use open and close PHP tags
+// if (isset($_REQUEST['name'],$_REQUEST['email'])) {
       
-    $name = $_REQUEST['name'];
-    $email = $_REQUEST['email'];
-    $message = $_REQUEST['message'];
+//     $name = $_REQUEST['name'];
+//     $email = $_REQUEST['email'];
+//     $message = $_REQUEST['message'];
       
-    // Set your email address where you want to receive emails. 
-    $to = 'davidtesno@gmail.com';
+//     // Set your email address where you want to receive emails. 
+//     $to = 'davidtesno@gmail.com';
       
-    $subject = 'Contact Request From Website';
-    $headers = "From: ".$name." <".$email."> \r\n";
+//     $subject = 'Contact Request From Website';
+//     $headers = "From: ".$name." <".$email."> \r\n";
       
-    $send_email = mail($to,$subject,$message,$headers);
+//     $send_email = mail($to,$subject,$message,$headers);
       
-    echo ($send_email) ? 'success' : 'error';
+//     echo ($send_email) ? 'success' : 'error';
       
-}
-?>
+// } -->
